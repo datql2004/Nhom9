91d95ca3ddfa67a52d01913f61faed817a9ff002
datql2004 <minhtrananh75@gmail.com>
2024-04-03 21:52:47 +0700

    adds

---
+++ CMU-CS 246 XIS/CustomerManagement/src/main/java/com/mycompany/customermanagement/CustomerManagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.customermanagement;
+
+/**
+ *
+ * @author DELL
+ */
+public class CustomerManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
